1d0c308cc21dbc8b4f3b084bcd553cddae48ceff
Owen Michuda <jamesom2@siebl-0224-37.ews.illinois.edu>
2020-12-07 15:26:18 -0600

    Setting up main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	// read data csv files
+	string connections_file = "oldenburg_road_network.csv";
+	string vertices_file = "banlk";
+
+	ifstream connections;
+
+	connections.open(connections_file);
+	while (!connections.eof()) {
+
+	}
+
+
+
+	// create map object from file
+	return 0;
+}
